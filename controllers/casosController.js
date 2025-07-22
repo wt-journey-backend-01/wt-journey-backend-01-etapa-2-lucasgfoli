1fa062bb100eea808f9d3ff4ef38ceb1c52f0617
lucasgabrielfer.oli@gmail.com <lucasgabrielfer.oli@gmail.com>
2025-07-22 17:06:54 -0300

    instalando dependencias

--- controllers/casosController.js
+++ controllers/casosController.js
@@ -1,10 +1,10 @@
-// const casosRepository = require("../repositories/casosRepository")
-// function getAllCasos(req, res) {
+const casosRepository = require("../repositories/casosRepository")
+function getAllCasos(req, res) {
 
-//         const casos = casosRepository.findAll()
-//         res.json(casos)
-// }
+        const casos = casosRepository.findAll()
+        res.json(casos)
+}
 
-// module.exports = {
-//    getAllCasos
-// }
+module.exports = {
+   getAllCasos
+}
